51f3856e1ce4b6d36d3ddc04c3eca6e24a467ad2
Maarten van den Hoek <maartenvandenhoek@gmail.com>
2012-10-18 01:25:36 +0200

    Added script to add sunpositions to mongo

--- src/storage/mongo.py
+++ src/storage/mongo.py
@@ -1,24 +1,57 @@
 from pymongo import Connection, GEO2D
+import datetime
+
+class HeightStorage():
+
+	def storeHeights(self):
+		height_file = "/media/Media/huf.txt"
+
+		db = Connection()['rotterdam']
+		db.places.create_index([("loc", GEO2D)], min=0 , max=88101)
 
 
-height_file = "/media/Media/huf.txt"
+		f = open(height_file)
+		for a in range(6):
+			print f.readline().replace(',', '.').split()
 
-db = Connection()['rotterdam']
-db.places.create_index([("loc", GEO2D)], min=0 , max=88101)
+		for y, line in enumerate(f):
+			heights = map(float, line.replace(',', '.').split())
+
+			bulk = []
+			for x, height in enumerate(heights):
+				if (height != -9999):
+					bulk.append({'loc': { 'x': x, 'y': y }, 'h' : height })
+			db.places.insert(bulk)	
+
+			if y == 5:
+				break
 
 
-f = open(height_file)
-for a in range(6):
-	print f.readline().replace(',', '.').split()
+class SunPositionStorage():
 
-for y, line in enumerate(f):
-	heights = map(float, line.replace(',', '.').split())
+	def storeSunPositions(self):
+		pos_file = "../../out.csv"
 
-	bulk = []
-	for x, height in enumerate(heights):
-		if (height != -9999):
-			bulk.append({'loc': { 'x': x, 'y': y }, 'h' : height })
-	db.places.insert(bulk)	
+		db = Connection()['sun_positions']
+		db.positions.create_index('datetime')
 
-	if y == 5:
-		break
+		f = open(pos_file)
+		f.readline() # skip first line
+		for a in range(20):	
+			row = f.readline().split(',')
+			year = int(row[1])
+			month = int(row[2])
+			day = int(row[3])
+			hour = int(row[4])
+			minute = int(row[5])*15
+			az = float(row[6])
+			el = float(row[7])
+
+			db.positions.insert({'datetime': datetime.datetime(year, month, day, hour, minute), 'el': el, 'az': az})
+
+		# print db.positions.find({ 'datetime': datetime.datetime(2012, 1, 1, 12, 15)})[0]
+
+
+a = SunPositionStorage()
+a.storeSunPositions()
+
