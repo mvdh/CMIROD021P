c0bde9a98c6b67147b80e11c47a15c336d6c8187
Maarten van den Hoek <maartenvandenhoek@gmail.com>
2012-11-13 19:08:48 +0100

    Added python script for sun position calculations

---
+++ src/preparation/height.py
@@ -0,0 +1,52 @@
+from pymongo import Connection, GEO2D
+import math
+
+class RDHeightStorage():
+
+    def __init__(self, zfile):
+        self.zfile = zfile # File with x, y and z values
+
+    def store_heights(self, box):
+        db      = 'rdam'
+        coll    = 'heights'
+
+        # Make connection with the database
+        db = Connection()[db]
+        db[coll].create_index([("loc", GEO2D)], min=0 , max=1000)
+        
+        # Use bulk insert to insert a lot of heights at once.
+        bulk = []
+
+        # Open file
+        f = open(self.zfile)
+        
+        # Skip header
+        f.readline()
+        for i, line in enumerate(f):
+            l = line.split(',')
+
+            # Py(mongo) can't handle large x and y values,
+            # so divide by 1000 to get smaller values.
+            x = math.floor(float(l[0]))/1000.0
+            y = math.floor(float(l[1]))/1000.0
+            z = float(l[2].rstrip())
+            
+            # Only store the height if if it is within the
+            # predifined box. 
+            if(self.is_within_box(x*1000, y*1000, box)):
+                bulk.append({'loc': { 'x': x, 'y': y }, 'z' : z })
+                if(len(bulk) >= 5000):
+                    # Store x, y, z in mongo
+                    db[coll].insert(bulk)
+                    bulk = []
+
+        # Save heights that are not saved yet
+        if(len(bulk) > 0):
+            db[coll].insert(bulk)
+
+        # Close file
+        f.close()
+
+    def is_within_box(self, x, y, box):
+        if box[0][0] <= x <= box[1][0] and box[0][1] <= y <= box[1][1]:
+            return True
---
+++ src/preparation/sun_position.py
@@ -0,0 +1,115 @@
+import math
+
+class SunPosition():
+
+    def sun_position(self, year, month, day=, hour=12, minute=0, sec=0, lat=51.923, lon=4.482):
+        twopi   = 2 * math.pi
+        deg2rad = math.pi / 180
+
+        month_days = [0,31,28,31,30,31,30,31,31,30,31,30]
+        day = day + self.cumsum(month_days)[month-1]
+        if year % 4 == 0 and (year % 400 == 0 or year % 100 != 0) and day >= 60 and not(month == 2 and day == 60): 
+            day = day + 1
+
+        # Get Julian date - 2400000
+        hour    = hour + minute / 60.0 + sec / 3600.0 # hour plus fraction
+        delta   = year - 1949
+        leap    = math.trunc(delta / 4) # former leapyears
+        jd      = 32916.5 + delta * 365 + leap + day + hour / 24.0
+        print hour, delta, leap, jd
+
+        # The input to the Atronomer's almanach is the difference between
+        # the Julian date and JD 2451545.0 (noon, 1 January 2000)
+        time = jd - 51545.0
+
+        # Ecliptic coordinates  
+
+        # Mean longitude
+        mnlong = 280.460 + 0.9856474 * time
+        mnlong = mnlong % 360
+        if mnlong < 0:
+            mnlong = mnlong + 360
+        print mnlong
+
+        # Mean anomaly
+        mnanom = 357.528 + 0.9856003 * time
+        mnanom = mnanom % 360
+        if mnanom < 0:
+            mnanom = mnanom + 360
+        mnanom = mnanom * deg2rad
+        print mnanom
+
+        # Ecliptic longitude and obliquity of ecliptic
+        eclong = mnlong + 1.915 * math.sin(mnanom) + 0.020 * math.sin(2 * mnanom)
+        eclong = eclong % 360
+        if eclong < 0:
+            eclong = eclong + 360
+        oblqec = 23.439 - 0.0000004 * time
+        eclong = eclong * deg2rad
+        oblqec = oblqec * deg2rad
+        print eclong, oblqec
+
+        # Celestial coordinates
+        # Right ascension and declination
+        num = math.cos(oblqec) * math.sin(eclong)
+        den = math.cos(eclong)
+        ra  = math.atan(num / den)
+        if den < 0:
+            ra = ra + math.pi
+        if den >= 0 and num < 0:
+            ra = ra + twopi
+        dec = math.asin(math.sin(oblqec) * math.sin(eclong))
+        print dec
+
+        # Local coordinates
+        # Greenwich mean sidereal time
+        gmst = 6.697375 + 0.0657098242 * time + hour
+        gmst = gmst % 24.0
+        if gmst < 0:
+            gmst = gmst + 24.0
+        print gmst
+
+        # Local mean sidereal time
+        lmst = gmst + lon / 15.0
+        lmst = lmst % 24.0
+        if lmst < 0:
+            lmst = lmst + 24.0
+        lmst = lmst * 15.0 * deg2rad
+
+        # Hour angle
+        ha = lmst - ra
+        if ha < -math.pi:
+            ha = ha + twopi
+        if ha > math.pi:
+            ha = ha - twopi
+        print ha
+
+        # Latitude to radians
+        lat = lat * deg2rad
+
+        # Azimuth and elevation
+        el = math.asin(math.sin(dec) * math.sin(lat) + math.cos(dec) * math.cos(lat) * math.cos(ha))
+        az = math.asin(-math.cos(dec) * math.sin(ha) / math.cos(el))        
+        print el, az
+
+        # For logic and names, see Spencer, J.W. 1989. Solar Energy. 42(4):353
+        cosAzPos = (0 <= math.sin(dec) - math.sin(el) * math.sin(lat))
+        sinAzNeg = (math.sin(az) < 0)
+        if cosAzPos and sinAzNeg:
+            az = az + twopi
+        if not cosAzPos:
+            az = math.pi - az
+
+        el  = el / deg2rad
+        az  = az / deg2rad
+        lat = lat / deg2rad
+
+        return dict(el=round(el, 4), az=round(az, 4))
+
+    def cumsum(self, it):
+        r = []
+        total = 0
+        for x in it:
+            total += x
+            r.append(total)
+        return r
